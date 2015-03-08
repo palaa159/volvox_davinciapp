31ad5a8d4e4c6fd3864abf2d0d4ed16ff01d0361
Apon Palanuwech <reach.apon@gmail.com>
2015-03-08 17:42:03 -0400

    Change to Mandrillapp

--- www/js/app.js
+++ www/js/app.js
@@ -22,232 +22,237 @@ var EVENT_BG;
 var EVENT_DISCLAIMER;
 
 angular.module(APP_NAME, [
-    'ionic',
-    'ngCordova',
-    'ngRetina',
-    'app.controllers',
-    'app.services'
+	'ionic',
+	'ngCordova',
+	'ngRetina',
+	'app.controllers',
+	'app.services'
 ])
 
 .run(function(
-    $ionicPlatform,
-    $rootScope,
-    $state,
-    $ionicViewSwitcher,
-    Dialogs,
-    Dropbox,
-    $cordovaDevice,
-    $cordovaSplashscreen,
-    $cordovaProgress,
-    $cordovaStatusbar) {
+	$ionicPlatform,
+	$rootScope,
+	$state,
+	$ionicViewSwitcher,
+	$timeout,
+	Dialogs,
+	Dropbox,
+	$cordovaDevice,
+	$cordovaSplashscreen,
+	$cordovaProgress,
+	$cordovaStatusbar) {
 
-    $ionicPlatform.ready(function() {
-        if (window.cordova && window.cordova.plugins.Keyboard) {
-            cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-        }
-        if (window.StatusBar) {
-            StatusBar.styleDefault();
-        }
-        if (window.cordova) {
-            $cordovaSplashscreen.show();
-            $cordovaStatusbar.show();
-            localStorage['uuid'] = $cordovaDevice.getUUID();
-        }
-        console.log('```` App Ready');
-        // SET GLOBAL
-        $rootScope.settings = {};
-        $rootScope.settings.event_folder = EVENT_FOLDER;
-        $rootScope.settings.dropbox_token = DROPBOX_TOKEN;
-        $rootScope.settings.postmark_email = POSTMARK_EMAIL;
-        $rootScope.settings.postmark_token = POSTMARK_TOKEN;
-        // GET ACCOUNT INFO
-        Dropbox.getAccountInfo(function(result) {
-            console.log(result);
-        });
-        Dropbox.getSettings(function(res) {
-            console.log(res);
+	$ionicPlatform.ready(function() {
+		if (window.cordova && window.cordova.plugins.Keyboard) {
+			cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+		}
+		if (window.StatusBar) {
+			StatusBar.styleDefault();
+		}
+		if (window.cordova) {
+			$cordovaStatusbar.show();
+			localStorage['uuid'] = $cordovaDevice.getUUID();
+		}
+		console.log('```` App Ready');
+		// SET GLOBAL
+		$rootScope.settings = {};
+		$rootScope.settings.event_folder = EVENT_FOLDER;
+		$rootScope.settings.dropbox_token = DROPBOX_TOKEN;
+		$rootScope.settings.postmark_email = POSTMARK_EMAIL;
+		$rootScope.settings.postmark_token = POSTMARK_TOKEN;
+		// GET ACCOUNT INFO
+		Dropbox.getAccountInfo(function(result) {
+			console.log(result);
+		});
+		Dropbox.getSettings(function(res) {
+			console.log(res);
 
-            EVENT_NAME = res.event_name;
-            EVENT_FOLDER = res.event_folder;
-            EVENT_BG = '/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/src_img/welcome_bg.jpg';
+			EVENT_NAME = res.event_name;
+			EVENT_FOLDER = res.event_folder;
+			EVENT_BG = '/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/src_img/welcome_bg.jpg';
 
-            Dropbox.returnDirectLink(EVENT_BG, function(d) {
-                $rootScope.backgroundBg = d;
-                console.log($rootScope.backgroundBg); // √
-                $state.go('/01-welcome');
-                // $state.go('/02-register');
-            });
-        }, function(err) {
-            console.log(err);
-            if (err == null) {
-                err = {};
-                err.error = 'No internet connection.';
-            }
-            /*
-                Cannot get settings file
-                – EVENT_FOLDER does not exist
-                – or, Internet connection lost
-            */
-            Dialogs.alert('settings.json | ' + err.error, 'OK', function() {
-                $state.go('/00-settings');
-            });
-        });
+			Dropbox.returnDirectLink(EVENT_BG, function(d) {
+				$rootScope.backgroundBg = d;
+				console.log($rootScope.backgroundBg); // √
+				$state.go('/01-welcome');
+				if(window.cordova) {
+					$timeout(function() {
+						$cordovaSplashscreen.hide();
+					}, 4000);
 
-    });
+				}
 
-    $rootScope.$on('$ionicView.afterEnter', function() {
-        // Any thing you can think of
-        if (window.cordova) {
-            $cordovaProgress.hide();
-            $cordovaSplashscreen.hide();
-        }
-        $('.image-lightbox').flipLightBox({
-            type: 'image',
-            when: {
-                opened: function(flb) {
-                    $rootScope.imgToShare = $($($(flb.cb).html()).children('.flb-front')).children('img').attr('src');
-                    $rootScope.msgToShare = "#hey #whatsup";
-                    $('#shareFacebook').on('flbClick', function() {
-                        $rootScope.socialToShare = 'Facebook';
-                        console.log('Share Facebook');
-                    });
+			});
+		}, function(err) {
+			console.log(err);
+			if (err == null) {
+				err = {};
+				err.error = 'No internet connection.';
+			}
+			/*
+			    Cannot get settings file
+			    – EVENT_FOLDER does not exist
+			    – or, Internet connection lost
+			*/
+			Dialogs.alert('settings.json | ' + err.error, 'OK', function() {
+				$state.go('/00-settings');
+			});
+		});
 
-                    $('#shareInstagram').on('flbClick', function() {
-                        $rootScope.socialToShare = 'Instagram';
-                        console.log('Share Instagram');
-                    });
+	});
 
-                    $('#shareMail').on('flbClick', function() {
-                        $rootScope.socialToShare = 'Email';
-                        console.log('Share Mail');
-                    });
+	$rootScope.$on('$ionicView.afterEnter', function() {
+		// Any thing you can think of
+		if (window.cordova) {
+			$cordovaProgress.hide();
+		}
+		$('.image-lightbox').flipLightBox({
+			type: 'image',
+			when: {
+				opened: function(flb) {
+					$rootScope.imgToShare = $($($(flb.cb).html()).children('.flb-front')).children('img').attr('src');
+					$rootScope.msgToShare = "#hey #whatsup";
+					$('#shareFacebook').on('flbClick', function() {
+						$rootScope.socialToShare = 'Facebook';
+						console.log('Share Facebook');
+					});
 
-                    $('#sharePrint').on('flbClick', function() {
-                        $rootScope.socialToShare = 'Print';
-                        console.log('Share Print');
-                    });
+					$('#shareInstagram').on('flbClick', function() {
+						$rootScope.socialToShare = 'Instagram';
+						console.log('Share Instagram');
+					});
 
-                    $('.share-icon', flb.backEl).on('flbClick', {
-                        flb: flb
-                    }, close);
+					$('#shareMail').on('flbClick', function() {
+						$rootScope.socialToShare = 'Email';
+						console.log('Share Mail');
+					});
 
-                    function close(e) {
-                        e.data.flb.close();
-                        $state.go('/04-share');
-                    }
-                }
-            }
-        });
-        console.log('```` View Updated.');
+					$('#sharePrint').on('flbClick', function() {
+						$rootScope.socialToShare = 'Print';
+						console.log('Share Print');
+					});
 
-    });
-    // Go to page x
-    $rootScope.goToPage = function(page) {
-        // Fade out
-        $ionicViewSwitcher.nextDirection('forward');
-        $state.go(page);
-    };
-    // Go to settings
-    $rootScope.goToSettings = function() {
-        console.log('```` Auth for settings');
-        // Prompt password
-        Dialogs.prompt('You shall not pass...',
-            function(result) {
-                if (result === APP_PASSWD) {
-                    // GO
-                    $state.go('/00-settings');
-                } else {
-                    // NOT GO
-                }
-            });
-    };
+					$('.share-icon', flb.backEl).on('flbClick', {
+						flb: flb
+					}, close);
+
+					function close(e) {
+						e.data.flb.close();
+						$state.go('/04-share');
+					}
+				}
+			}
+		});
+		console.log('```` View Updated.');
+
+	});
+	// Go to page x
+	$rootScope.goToPage = function(page) {
+		// Fade out
+		$ionicViewSwitcher.nextDirection('forward');
+		$state.go(page);
+	};
+	// Go to settings
+	$rootScope.goToSettings = function() {
+		console.log('```` Auth for settings');
+		// Prompt password
+		Dialogs.prompt('You shall not pass...',
+			function(result) {
+				if (result === APP_PASSWD) {
+					// GO
+					$state.go('/00-settings');
+				} else {
+					// NOT GO
+				}
+			});
+	};
 })
 
 .config(function(
-    $ionicConfigProvider,
-    $httpProvider,
-    $cordovaInAppBrowserProvider,
-    $stateProvider,
-    $urlRouterProvider,
-    $sceDelegateProvider) {
+	$ionicConfigProvider,
+	$httpProvider,
+	$cordovaInAppBrowserProvider,
+	$stateProvider,
+	$urlRouterProvider,
+	$sceDelegateProvider) {
 
-    /*
-      Configurations
-    */
-    $sceDelegateProvider.resourceUrlWhitelist(['**']);
-    $ionicConfigProvider.views.maxCache(0);
-    $httpProvider.defaults.cache = false;
-    var options = {
-        location: 'yes',
-        clearcache: 'yes',
-        toolbar: 'no'
-    };
-    $cordovaInAppBrowserProvider.setDefaultOptions(options);
+	/*
+	  Configurations
+	*/
+	$sceDelegateProvider.resourceUrlWhitelist(['**']);
+	$ionicConfigProvider.views.maxCache(0);
+	$httpProvider.defaults.cache = false;
+	var options = {
+		location: 'yes',
+		clearcache: 'yes',
+		toolbar: 'no'
+	};
+	$cordovaInAppBrowserProvider.setDefaultOptions(options);
 
-    /*
-      Routing
-    */
+	/*
+	  Routing
+	*/
 
-    $stateProvider
-        .state('/00-settings', {
-            url: "/00-settings",
-            templateUrl: "views/00-settings.html",
-            controller: 'SettingsCtrl'
-        })
-        .state('/01-welcome', {
-            url: "/01-welcome",
-            templateUrl: "views/01-welcome.html",
-            controller: 'WelcomeCtrl'
-        })
-        .state('/02-register', {
-            url: "/02-register",
-            templateUrl: "views/02-register.html",
-            controller: 'RegisterCtrl'
-        })
-        .state('/03-gallery', {
-            url: "/03-gallery",
-            templateUrl: "views/03-gallery.html",
-            controller: 'GalleryCtrl'
-        })
-        .state('/04-share', {
-            url: "/04-share",
-            templateUrl: "views/04-share.html",
-            controller: 'ShareCtrl'
-        })
-        .state('/05-thankyou', {
-            url: "/05-thankyou",
-            templateUrl: "views/05-thankyou.html",
-            controller: 'ThankYouCtrl'
-        });
+	$stateProvider
+		.state('/00-settings', {
+			url: "/00-settings",
+			templateUrl: "views/00-settings.html",
+			controller: 'SettingsCtrl'
+		})
+		.state('/01-welcome', {
+			url: "/01-welcome",
+			templateUrl: "views/01-welcome.html",
+			controller: 'WelcomeCtrl'
+		})
+		.state('/02-register', {
+			url: "/02-register",
+			templateUrl: "views/02-register.html",
+			controller: 'RegisterCtrl'
+		})
+		.state('/03-gallery', {
+			url: "/03-gallery",
+			templateUrl: "views/03-gallery.html",
+			controller: 'GalleryCtrl'
+		})
+		.state('/04-share', {
+			url: "/04-share",
+			templateUrl: "views/04-share.html",
+			controller: 'ShareCtrl'
+		})
+		.state('/05-thankyou', {
+			url: "/05-thankyou",
+			templateUrl: "views/05-thankyou.html",
+			controller: 'ThankYouCtrl'
+		});
 })
 
 // Filters
 
 .filter('trusted', ['$sce',
-    function($sce) {
-        return function(url) {
-            return $sce.trustAsResourceUrl(url);
-        };
-    }
+	function($sce) {
+		return function(url) {
+			return $sce.trustAsResourceUrl(url);
+		};
+	}
 ]);
 
 Array.prototype.chunk = function(chunkSize) {
-    var array = this;
-    return [].concat.apply([],
-        array.map(function(elem, i) {
-            return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
-        })
-    );
+	var array = this;
+	return [].concat.apply([],
+		array.map(function(elem, i) {
+			return i % chunkSize ? [] : [array.slice(i, i + chunkSize)];
+		})
+	);
 };
 
 function getEventName() {
-    return EVENT_NAME;
+	return EVENT_NAME;
 }
 
 function getEventFolder() {
-    return EVENT_FOLDER;
+	return EVENT_FOLDER;
 }
 
 function getEventDisclaimer() {
-    return EVENT_DISCLAIMER;
+	return EVENT_DISCLAIMER;
 }
--- www/js/services.js
+++ www/js/services.js
@@ -2,129 +2,114 @@ angular.module('app.services', [])
 
 /* Dropbox */
 .factory('Dropbox', function($http, $rootScope) {
-    var service = {};
+	var service = {};
 
-    service.getAccountInfo = function(cb) {
-        $http.get('https://api.dropbox.com/1/account/info', {
-            headers: {
-                'Authorization': 'Bearer ' + DROPBOX_TOKEN
-            }
-        }).then(function(result) {
-            cb(result.data);
-        });
-    };
+	service.getAccountInfo = function(cb) {
+		$http.get('https://api.dropbox.com/1/account/info', {
+			headers: {
+				'Authorization': 'Bearer ' + DROPBOX_TOKEN
+			}
+		}).then(function(result) {
+			cb(result.data);
+		});
+	};
 
-    service.getSettings = function(success, fail) {
-        console.log('````` Get settings.json');
-        $http.get('https://api-content.dropbox.com/1/files/auto/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/settings.json', {
-            headers: {
-                'Authorization': 'Bearer ' + DROPBOX_TOKEN
-            }
-        })
-        .success(function(res) {
-            success(res);
-        })
-        .error(function(err) {
-            fail(err);
-        });
-    };
+	service.getSettings = function(success, fail) {
+		console.log('````` Get settings.json');
+		$http.get('https://api-content.dropbox.com/1/files/auto/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/settings.json', {
+				headers: {
+					'Authorization': 'Bearer ' + DROPBOX_TOKEN
+				}
+			})
+			.success(function(res) {
+				success(res);
+			})
+			.error(function(err) {
+				fail(err);
+			});
+	};
 
-    service.getImages = function(cb) {
-        $http.get('https://api.dropbox.com/1/metadata/auto/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/social_gallery', {
-            headers: {
-                'Authorization': 'Bearer ' + DROPBOX_TOKEN
-            }
-        }).then(function(result) {
-            // return links via Dropbox media api
-            $rootScope.gallery = [];
-            // TODO: Async
-            var i = 0;
-            async.each(result.data.contents, function(content, callback) {
-                function callback() {
-                    // console.log(result.data.contents.length, i);
-                    if (i === result.data.contents.length - 1) {
-                        // console.log('Done');
-                        if (cb) cb();
-                    } else {
-                        i++;
-                    }
-                }
-                service.returnDirectLink(content.path, function(result) {
-                    $rootScope.gallery.push(result);
-                    callback();
-                });
-            }, function(err) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        });
-    };
+	service.getImages = function(cb) {
+		$http.get('https://api.dropbox.com/1/metadata/auto/' + DROPBOX_FOLDER + '/' + getEventFolder() + '/social_gallery', {
+			headers: {
+				'Authorization': 'Bearer ' + DROPBOX_TOKEN
+			}
+		}).then(function(result) {
+			// return links via Dropbox media api
+			$rootScope.gallery = [];
+			// TODO: Async
+			var i = 0;
+			async.each(result.data.contents, function(content, callback) {
+				function callback() {
+					// console.log(result.data.contents.length, i);
+					if (i === result.data.contents.length - 1) {
+						// console.log('Done');
+						if (cb) cb();
+					} else {
+						i++;
+					}
+				}
+				service.returnDirectLink(content.path, function(result) {
+					$rootScope.gallery.push(result);
+					callback();
+				});
+			}, function(err) {
+				if (err) {
+					console.log(err);
+				}
+			});
+		});
+	};
 
-    service.appendUser = function(cb) {
+	service.appendUser = function(cb) {
 
-    };
+	};
 
-    // Helper
-    service.returnDirectLink = function(path, cb) {
-        // "/davinci_app/test_event/photobooth/6a010535647bf3970b015390876439970b-500wi.jpg"
-        var link;
-        $http.get('https://api.dropbox.com/1/media/auto' + path, {
-            headers: {
-                'Authorization': 'Bearer ' + DROPBOX_TOKEN
-            }
-        }).then(function(result) {
-            if (cb) cb(result.data.url);
-        });
-    };
+	// Helper
+	service.returnDirectLink = function(path, cb) {
+		// "/davinci_app/test_event/photobooth/6a010535647bf3970b015390876439970b-500wi.jpg"
+		var link;
+		$http.get('https://api.dropbox.com/1/media/auto' + path, {
+			headers: {
+				'Authorization': 'Bearer ' + DROPBOX_TOKEN
+			}
+		}).then(function(result) {
+			if (cb) cb(result.data.url);
+		});
+	};
 
-    return service;
+	return service;
 })
 
 /* Postmarkapp */
-.factory('Postmark', function($http) {
-    var service = {};
+.factory('Postmark', function() {
+	var service = {};
+	service.sendMail = function(name, email, cb) {
 
-    service.sendMail = function(name, email, cb) {
-        $http.post('https://api.postmarkapp.com/email', {
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json',
-                'X-Postmark-Server-Token': POSTMARK_TOKEN
-            },
-            data: {
-                'From': POSTMARK_EMAIL,
-                'To': email,
-                'Subject': 'Thank you for registering Davinci',
-                'HtmlBody': ''
-            }
-        }).then(function(response) {
-            if (cb) cb(response);
-        });
-    };
+	};
 
-    return service;
+	return service;
 })
 
 /* Dialogs */
 .factory('Dialogs', function($ionicPopup, $cordovaDialogs) {
-    var service = {};
+	var service = {};
 
-    service.alert = function(message, buttonName, cb) {
-        $cordovaDialogs.alert(message, getEventName(), buttonName).then(function() {
-            if (cb) cb();
-        });
-    };
-    service.prompt = function(msg, cb) {
-        $ionicPopup.prompt({
-            title: 'Password Check',
-            template: 'Enter your secret password',
-            inputType: 'password',
-            inputPlaceholder: 'Your password'
-        }).then(function(res) {
-            cb(res);
-        });
-    };
+	service.alert = function(message, buttonName, cb) {
+		$cordovaDialogs.alert(message, getEventName(), buttonName).then(function() {
+			if (cb) cb();
+		});
+	};
+	service.prompt = function(msg, cb) {
+		$ionicPopup.prompt({
+			title: 'Password Check',
+			template: 'Enter your secret password',
+			inputType: 'password',
+			inputPlaceholder: 'Your password'
+		}).then(function(res) {
+			cb(res);
+		});
+	};
 
-    return service;
+	return service;
 });
